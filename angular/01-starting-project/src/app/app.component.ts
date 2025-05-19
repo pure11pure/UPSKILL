d5ba5db9172c1829ff6e7a63de455fe70100151d
Pure <jpspure@gmail.com>
2025-05-19 20:05:59 +0700

    angular-update: ngFor, ngIf

--- angular/01-starting-project/src/app/app.component.ts
+++ angular/01-starting-project/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './USER_DUMMY';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
