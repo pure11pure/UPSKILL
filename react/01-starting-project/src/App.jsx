6ecfe1bf2b9c6a72b9a19a2ac6abcc66bc2b87ac
Pure <jpspure@gmail.com>
2025-05-27 12:29:24 +0700

    react-update: components

--- react/01-starting-project/src/App.jsx
+++ react/01-starting-project/src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from './assets/react-core-concepts.png'
+
 import { CORE_CONCEPTS } from './data.js'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length)]
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { Header } from './components/Header.jsx';
+import { CoreConcept } from './components/CoreConcept.jsx';
 
 function App() {
   return (
