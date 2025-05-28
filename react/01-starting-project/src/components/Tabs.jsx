56850cc5677acbde4a747e953efabd8476d66b60
Pure <jpspure@gmail.com>
2025-05-28 13:01:41 +0700

    react-update: 01-starting-project

--- react/01-starting-project/src/components/Tabs.jsx
+++ react/01-starting-project/src/components/Tabs.jsx
@@ -1,8 +1,8 @@
-export default function Tabs({ children, buttons }) {
-    return (<>
-        <menu>
-            {buttons}
-        </menu>
-        {children}
-    </>)
+export default function Tabs({ children, buttons, ButtonContainer = "menu" }) {
+    return (
+        <>
+            <ButtonContainer>{buttons}</ButtonContainer>
+            {children}
+        </>
+    )
 }
