095801b15bc905a86ca6926654e7e4ec2ff17386
Pure <jpspure@gmail.com>
2025-05-28 11:17:06 +0700

    react-update: split Section

--- react/01-starting-project/src/components/Examples.jsx
+++ react/01-starting-project/src/components/Examples.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
 import { EXAMPLES } from '../data.js';
 
 export default function Examples() {
@@ -26,8 +27,7 @@ export default function Examples() {
     }
 
     return (
-        <section id="examples">
-            <h2>Examples</h2>
+        <Section title={'Examples'} id="examples">
             <menu>
                 <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')} >Components</TabButton>
                 <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')} >JSX</TabButton>
@@ -35,6 +35,6 @@ export default function Examples() {
                 <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')} >State</TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     )
 }
