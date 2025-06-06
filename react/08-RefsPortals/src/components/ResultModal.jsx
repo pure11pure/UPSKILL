1f052844c85e0e11e0d5373912ca22588bcb207f
Pure <jpspure@gmail.com>
2025-06-06 12:56:05 +0700

    react-update (08) : Portal

--- react/08-RefsPortals/src/components/ResultModal.jsx
+++ react/08-RefsPortals/src/components/ResultModal.jsx
@@ -1,24 +1,44 @@
 /* ในกรณี angular version ต่ำกว่า 19 จะใช้ ref ไม่ได้ */
 
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
-  ref
-) {
-  return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
+
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  const score = Math.round((1 - (remainingTime / (targetTime * 1000))) * 100);
+  //ยิ่งใกล้หมดเวลาคะแนนยิ่งเยอะ
+
+  useImperativeHandle(ref, () => {
+    //TimerChallenge สามารถเข้าถึงได้แค่ ใช้ .open เพราะมันถูก Encapsulations
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  })
+
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score} </h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 });
 
--- react/08-RefsPortals/src/components/TimerChallenge.jsx
+++ react/08-RefsPortals/src/components/TimerChallenge.jsx
@@ -5,40 +5,68 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timeExpired, setTimeExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimeExpired(true);
-      dialog.current.showModal();
-    }, targetTime * 1000);
-
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10)
+    }, 10); // 10 ms
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />
       <section className="challenge">
         <h2>{title}</h2>
-        {timeExpired && <p>You lost!</p>}
+        {/* {timeExpired && <p>You lost!</p>} */}
         <p className="Challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
         <p className="">
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
   );
 }
+
+
+/**
+ setTimeout(() => {
+  console.log("Hello after 3 seconds");
+}, 3000);
+
+พอครบ 3 วินาที console.log() จะทำงาน ครั้งเดียว
+
+*/
+
+
+/*
+setInterval(() => {
+  console.log("Tick every second");
+}, 1000);
+
+→ console.log() จะทำงาน ทุก ๆ 1 วินาที ซ้ำ ๆ ไปเรื่อย ๆ
+(จนกว่าเราจะหยุดด้วย clearInterval())
+ */
