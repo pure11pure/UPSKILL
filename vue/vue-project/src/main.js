1f9139ec4fe2af02e773edb3b90edb3a680d5570
Pure <jpspure@gmail.com>
2025-06-12 13:39:18 +0700

    vue-update: vue-project (import)

--- vue/vue-project/src/main.js
+++ vue/vue-project/src/main.js
@@ -3,5 +3,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import App2 from './App2.vue'
+import App3 from './App3.vue'
+
 
 createApp(App).mount('#app')
