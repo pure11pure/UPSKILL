f9862a62b404abfa4c78b259496cf7fcaed9c907
Pure <jpspure@gmail.com>
2025-06-17 12:48:57 +0700

    php-update (00) : $_GET and $_POST (1)

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,26 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <form action="index.php" method="get"> -->
+    <form action="index.php" method="post">
+        <label>username: </label><br>
+        <input type="text" name="username"><br>
+        <label>password: </label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="log in">
+    </form>
+</body>
+
+</html>
+
 <?php
-   $x = 10;
-   $y = 3;
-   $z = null;
 
-//    $z = $x + $y;
-//    $z = $x - $y;
-//    $z = $x * $y;
-//    $z = $x / $y;
-//    $z = $x ** $y;
-//    $z = $x % $y;
-//    echo $z;
+    //$_GET =>  Date is appended to the url
+    //          NOT SECURE 
+    //          GET requests can be cached
+    //          Better for a search page
 
-    $counter = 5;
+    // echo "{$_GET["username"]} <br>" ;
+    // echo "{$_GET["password"]} <br>"; 
+    
+    //$_POST =>  Date is packaged inside the body of the HTTP request
+    //          MORE SECURE 
+    //          GET requests are not cached
+    //          Better for submitting credentials  
 
-    // $counter = $counter + 1;
-    // $counter++;
-    // $counter--;
-    // $counter+=3;
-    // $counter-=3;
-    // echo $counter; 
-
-    $total = 1 + 2 -3 *4 / 5 ** 6;
-
-    echo $total;
+    echo "{$_POST["username"]} <br>" ;
+    echo "{$_POST["password"]} <br>";
 ?>
