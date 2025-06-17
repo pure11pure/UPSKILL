28626a8d911bc46aa1e22c71d0cc5e4fc70a9695
Pure <jpspure@gmail.com>
2025-06-17 13:31:38 +0700

    php-update (00) : if statements (2)

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,16 +1,33 @@
-<?php
-    $age = 101;
+<?php 
 
-    if($age >= 100){
-        echo "You are too old to enter this site";
+    $hours = 50;
+    $rate = 15;
+    $weekly_pay = null;
+    
+    if($hours <= 0){
+        $weekly_pay = 0;
     }
-    elseif($age <= 0){
-        echo "That wasn't a valid age";
-    }
-    elseif($age >= 18){
-        echo "You may enter this site";
+    elseif($hours <= 40){
+        $weekly_pay = $hours * $rate;
     }
     else{
-        echo "You must be 18+ to enter";
+        $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5) );
     }
+
+    echo "You made {$weekly_pay} this week";
 ?>
+
+
+
+<!-- <?php
+  
+  $adult = true;
+
+  if($adult == true){
+    echo "You may enter this site";
+  }
+  else{
+    echo "You must be an adult to enter";
+  }
+
+?> -->
