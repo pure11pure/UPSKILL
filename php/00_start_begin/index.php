f4e8d98388f51d6cae583bcde1f48ae300fee64c
Pure <jpspure@gmail.com>
2025-06-18 13:17:54 +0700

    php-update (00) : while loops

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,84 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label>Enter a number to count down from : </label>
-        <input type="text" name="counter" />
-        <input type="submit" value="start" />
-    </form>
-</body>
-
-</html>
-
-<?php
-
-    if (isset($_POST["counter"])) {
-        $counter = $_POST["counter"];
-
-        for ($i = $counter; $i > 0; $i--) {
-            echo $i . "<br>";
-        }
-    }
-
-?>
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label>Enter a number to count to : </label>
-        <input type="text" name="counter" />
-        <input type="submit" value="start" />
-    </form>
-</body>
-
-</html>
-
-<?php
-
-    if (isset($_POST["counter"])) {
-        $counter = $_POST["counter"];
-
-        for ($i = 1; $i <= $counter; $i++) {
-            echo $i . "<br>";
-        }
-    }
-
-?> -->
-
-
-
 <!-- <?php
 
-        // for($i = 1; $i <= 10; $i++){
-        //     echo  $i ."<br>";
-        // }
+        $counter = 0;
 
-        // for($i = 1; $i <= 10; $i+=2){
-        //     echo  $i ."<br>";
-        // }
-
-        // for($i = 10; $i > 0; $i--){
-        //     echo  $i ."<br>";
-        // }
-
-        for ($i = 10; $i > 0; $i -= 2) {
-            echo  $i . "<br>";
+        while ($counter <= 10) {
+            $counter++;
+            echo $counter . "<br>";
         }
+
         ?> -->
