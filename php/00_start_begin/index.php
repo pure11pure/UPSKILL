4e45b45cd3213240a9f077ebf86cc4d23e42c43f
Pure <jpspure@gmail.com>
2025-06-18 13:06:41 +0700

    php-update (00) : for loops

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,89 +1,84 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-    $date = date("l");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    switch($date){
-        case "Monday":
-            echo "I hate Mondays.";
-            break;
-        case "Tuesday":
-            echo "It is Taco Tuesday.";
-            break;
-        case "Wednesday":
-            echo "The work week is half over!";
-            break;
-        case "Thursday":
-            echo "It's almost the weekend.";
-            break;
-        case "Friday":
-            echo "The weenend is here!";
-            break;
-        case "Saturday":
-            echo "Time to party!";
-            break;
-        case "Sunday":
-            echo "Time to relax.";
-            break;
-        default:
-            echo "{$date} is not a day";
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a number to count down from : </label>
+        <input type="text" name="counter" />
+        <input type="submit" value="start" />
+    </form>
+</body>
+
+</html>
+
+<?php
+
+    if (isset($_POST["counter"])) {
+        $counter = $_POST["counter"];
+
+        for ($i = $counter; $i > 0; $i--) {
+            echo $i . "<br>";
+        }
     }
 
 ?>
 
 
 
-<!-- <?php 
+<!-- <!DOCTYPE html>
+<html lang="en">
 
-// ถ้าไม่ break มันจะทำตามเคสข้างล่างจากมันทั้งหมด
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-      $grade = "B";
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a number to count to : </label>
+        <input type="text" name="counter" />
+        <input type="submit" value="start" />
+    </form>
+</body>
 
-      switch($grade){
-        case "A" : 
-            echo "You did great.";
-            break;
-        case "B" : 
-            echo "You did good.";
-            break;
-        case "C" : 
-            echo "You did okay.";
-            break;
-        case "D" : 
-            echo "You did poorly.";
-            break;
-        case "F" : 
-            echo "You failed.";
-            break;
-        default: 
-            echo "{$grade} is not valid.";
-      }
+</html>
+
+<?php
+
+    if (isset($_POST["counter"])) {
+        $counter = $_POST["counter"];
+
+        for ($i = 1; $i <= $counter; $i++) {
+            echo $i . "<br>";
+        }
+    }
 
 ?> -->
-
 
 
 
 <!-- <?php
 
-    $grade = "A";
+        // for($i = 1; $i <= 10; $i++){
+        //     echo  $i ."<br>";
+        // }
 
-    if ($grade == "A") {
-        echo "You did great";
-    } 
-    elseif ($grade == "B") {
-        echo "You did good";
-    } 
-    elseif ($grade == "c") {
-        echo "You did okay";
-    } 
-    elseif ($grade == "d") {
-        echo "You did poorly";
-    } 
-    elseif ($grade == "F") {
-        echo "You failed";
-    }
-    else{
-        echo "{$grade} is not a valid grade.";
-    }
+        // for($i = 1; $i <= 10; $i+=2){
+        //     echo  $i ."<br>";
+        // }
 
-?> -->
+        // for($i = 10; $i > 0; $i--){
+        //     echo  $i ."<br>";
+        // }
+
+        for ($i = 10; $i > 0; $i -= 2) {
+            echo  $i . "<br>";
+        }
+        ?> -->
