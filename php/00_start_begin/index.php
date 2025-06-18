0de41dd3c6f007cb874258c890a2f1ca6fb9fc20
Pure <jpspure@gmail.com>
2025-06-18 13:38:39 +0700

    php-update (00) : arrays

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,10 +1,27 @@
-<!-- <?php
+<?php 
 
-        $counter = 0;
+    // $food_1 = "apple";
+    // $food_2 = "orange";
+    // $food_3 = "banana";
+    // $food_4 = "coconut";
 
-        while ($counter <= 10) {
-            $counter++;
-            echo $counter . "<br>";
-        }
+    $foods = array("apple", "orange", "banana", "coconut");
 
-        ?> -->
+    // echo $foods[0] . "<br>";
+    // echo $foods[1] . "<br>";
+    // echo $foods[2] . "<br>";
+    // echo $foods[3] . "<br>";
+
+    // array_push($foods, "pineapple", "kiwi");
+    // array_pop($foods);
+    // array_shift($foods);
+    // $foods = array_reverse($foods);
+    // echo count($foods);
+
+    foreach($foods as $food){
+        echo $food . "<br>";
+    }
+
+
+
+?>
