0fca026d3c179734816b65bae90d9cd02e1a122a
Pure <jpspure@gmail.com>
2025-06-19 12:41:13 +0700

    php-update (00) :  $_COOKIE

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,26 +1,23 @@
 <?php 
 
-    include("header.html");
-?>
+    // setcookie("fav_food", "pizza", time() + (86400 *2), "/");
+    setcookie("fav_food", "pizza", time() + 60 , "/"); // 60 วินาที
+    setcookie("fav_drink", "coffee", time() + (86400 *3), "/"); // 2 วัน
+    setcookie("fav_dessert", "ice cream", time() + (86400 *4), "/");
 
-<!DOCTYPE html>
-<html lang="en">
+    // foreach($_COOKIE as $key => $value){
+    //     echo "$key = $value <br>"; 
+    // }
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    if(isset($_COOKIE["fav_food"])){
+        echo "BUY SOME {$_COOKIE["fav_food"]} !!!";
+    }
+    else {
+        echo "I don't know your favorite food";
+    }
 
-<body>
-    This is the <b>Home</b> Page<br>
-    Stuff about your home page can go here<br>
-</body>
 
-</html>
 
-<?php 
 
-    include("footer.html");
 
 ?>
