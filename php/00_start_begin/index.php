c06ebbf33b8c7f3fa244471ca282c006875fe8ca
Pure <jpspure@gmail.com>
2025-06-20 10:37:41 +0700

    php-update (00) : PHP Connect to MySQL database

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -1,16 +1,21 @@
 <?php 
 
-    $password = "pizza";
-
-    $hash = password_hash($password , PASSWORD_DEFAULT);
-
-    // echo $hash;
-
-    if(password_verify("pizza1", $hash)){
-        echo "You are Logged in!";
-    }
-    else {
-        echo "Incorrect password!";
-    }
+    include("database.php");
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Hello
+</body>
+
+</html>
