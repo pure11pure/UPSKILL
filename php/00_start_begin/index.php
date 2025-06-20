2627d2812adeb192c35477fdfbff64468a482017
Pure <jpspure@gmail.com>
2025-06-20 10:47:56 +0700

    php-update (00) : PHP insert into MySQL database

--- php/00_start_begin/index.php
+++ php/00_start_begin/index.php
@@ -2,20 +2,21 @@
 
     include("database.php");
 
+    $username = "piky";
+    $password = "piky1";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (user, password)
+            VALUES ('$username', '$hash')";
+
+    try{
+            mysqli_query($conn, $sql);
+            echo "User is now registered";
+    }
+    catch(mysqli_sql_exception){
+        echo "Could not register user";
+    }
+
+        mysqli_close($conn);
+
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    Hello
-</body>
-
-</html>
